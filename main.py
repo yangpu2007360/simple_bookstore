fb17217a88f59a2071f5438348db884e2033f993
yangpu2007360 <yangpu2007360@gmail.com>
2022-05-14 18:05:57 -0700

    updated

--- main.py
+++ main.py
@@ -295,6 +295,5 @@ def create_checkout_session():
 
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(host=os.getenv('IP', '0.0.0.0'),
-            port=int(os.getenv('PORT', 4444)))
+    app.run(debug=True)
+
